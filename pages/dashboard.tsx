e74fe2841b96bab86b1706497f575805be982c34
Aitsam Ahad <aitsam.ahad@gmail.com>
2022-01-18 13:33:04 +0500

    Created dashboard page.

---
+++ pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import DashboardTemplate from "@templates/Dashboard"
+import type { NextPage } from "next"
+
+const Dashboard: NextPage = () => {
+	return <DashboardTemplate />
+}
+
+export default Dashboard
